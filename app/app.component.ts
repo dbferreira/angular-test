f1482c0e2cec71277214e78b0cea4d8918b030cf
Daniel Ferreira <DanielF@imqs.local>
2016-06-09 12:23:30 +0200

    no message

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,32 +1,21 @@
-import { Component } from '@angular/core';
-
-
-export class Hero {
-	id: number;
-	name: string;
-}
-
+import { Component }       from '@angular/core';
+import { HeroService }     from './hero.service';
+import { HeroesComponent } from './heroes.component';
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router-deprecated';
 
 
 @Component({
   selector: 'my-app',
-  template: `<h1>{{title}}</h1>
-  			<h2>{{hero.name}} details!</h2>
-  			<div>
-  				<label>id: </label>
-  				{{hero.id}}
-  			</div>
-  			<div>
-  				<label>name: </label>
-  				<input [(ngModel)]="hero.name" placeholder="name">
-  			</div>`
+  template: `
+    <h1>{{title}}</h1>
+    <my-heroes></my-heroes>
+  `,
+  directives: [ROUTER_DIRECTIVES],
+  providers: [
+    ROUTER_PROVIDERS,
+    HeroService
+  ]
 })
-
-
 export class AppComponent {
-	title = "Tour of Heroes";
-	hero: Hero = {
-		id: 1,
-		name: "Windstorm"
-	};
- }
+  title = 'Tour of Heroes';
+}
